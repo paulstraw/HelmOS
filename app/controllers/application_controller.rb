7244d237809146b23b1e01cd619a6cbc7d17cfd3
Paul Straw <paulstraw@paulstraw.com>
2014-12-23 03:50:10 -0800

    No SSL in staging (for now)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  force_ssl if Rails.env.production? || Rails.env.staging?
+  force_ssl if Rails.env.production?
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
