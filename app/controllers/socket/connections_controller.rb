f2d6228d6764b00a87cccae4f3421eee46394c38
Paul Straw <paulstraw@paulstraw.com>
2014-12-30 01:54:50 -0800

    Ding.

--- app/controllers/socket/connections_controller.rb
+++ app/controllers/socket/connections_controller.rb
@@ -5,14 +5,20 @@ class Socket::ConnectionsController < WebsocketRails::BaseController
     ship = current_user.current_ship
 
     ship.update_attribute :connected, true
-    WebsocketRails[ship.currently_orbiting.channel_name].trigger :ship_arrived, ship
+    WebsocketRails[ship.currently_orbiting.channel_name].trigger :ship_arrived, ship.as_json(
+      include: [:faction],
+      methods: [:name_degrees, :orbit_distance_multiplier, :orbit_time_multiplier]
+    )
   end
 
   def disconnected
     ship = current_user.current_ship
 
     ship.update_attribute :connected, false
-    WebsocketRails[ship.currently_orbiting.channel_name].trigger :ship_departed, ship
+    WebsocketRails[ship.currently_orbiting.channel_name].trigger :ship_departed, ship.as_json(
+      include: [:faction],
+      methods: [:name_degrees, :orbit_distance_multiplier, :orbit_time_multiplier]
+    )
   end
 
   def authorize_private_channel
